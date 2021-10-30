e8c8a6dd23f54099d88e0122bb159894feedab1c
Trac3D1y <i2rys@protonmail.com>
2021-10-31 06:48:38 +0800

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,7 @@
+//Dependencies
+const IMSad = require("../index.js")
+
+//Main
+IMSad.self("images", "Blackpink Lisa", 10, function(callback){ //Absolutely not a KPOP lover, It's just for testing but who cares.
+    console.log("Finished scraping & downloading.")
+})
